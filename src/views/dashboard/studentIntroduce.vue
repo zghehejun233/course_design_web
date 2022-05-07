5c517e2ca62d65e3dd88e29e70954c833c510602
guosurui <13012615613@163.com>
2022-05-07 20:17:49 +0800

    feat: 添加一个饼
    用来展示学生的生活侧重

--- src/views/dashboard/studentIntroduce.vue
+++ src/views/dashboard/studentIntroduce.vue
@@ -1,11 +1,11 @@
 <template>
   <el-container class="el-container" style="height: 100%; width: 100%">
     <el-aside width="200px">
-      <Navi/>
+      <Navi />
     </el-aside>
     <el-container>
       <el-header
-          style="
+        style="
           height: 60px;
           background-color: #409eff;
           padding: 15px;
@@ -14,7 +14,7 @@
         "
       >
         <div
-            style="
+          style="
             font-size: 24px;
             color: white;
             letter-spacing: 6px;
@@ -29,8 +29,8 @@
           <el-card class="box-card">
             <div class="card-header">基本信息</div>
             <div
-                class="item"
-                style="
+              class="item"
+              style="
                 display: flex;
                 flex-direction: row;
                 align-content: center;
@@ -39,13 +39,13 @@
             >
               <div class="item header-img" style="width: 180px">
                 <el-image
-                    src="https://gravatar.loli.net/avatar/c14402ce10441a645a6151118eef7609?d=retro&v=1.4.18"
-                    fit="fill"
+                  src="https://gravatar.loli.net/avatar/c14402ce10441a645a6151118eef7609?d=retro&v=1.4.18"
+                  fit="fill"
                 />
               </div>
               <div
-                  class="item"
-                  style="
+                class="item"
+                style="
                   display: flex;
                   flex-direction: column;
                   align-content: center;
@@ -60,8 +60,8 @@
 
           <el-card class="box-card">
             <div
-                class="item"
-                style="
+              class="item"
+              style="
                 display: flex;
                 flex-direction: column;
                 align-content: start;
@@ -69,11 +69,11 @@
               "
             >
               <el-row
-                  type="flex"
-                  style="margin-left: 12px; margin-right: 12px; width: 100%"
-                  :gutter="110"
-                  v-for="(item, i) in attachList"
-                  :key="i"
+                type="flex"
+                style="margin-left: 12px; margin-right: 12px; width: 100%"
+                :gutter="110"
+                v-for="(item, i) in attachList"
+                :key="i"
               >
                 <p>{{ item.title }}</p>
                 <p v-html="item.content"></p>
@@ -82,27 +82,49 @@
           </el-card>
           <el-card class="box-card">
             <div
-                style="
-              display: flex;
-              flex-direction: column;
-              align-content: center;
-              align-items: center;
-            "
+              style="
+                display: flex;
+                flex-direction: column;
+                align-content: center;
+                align-items: center;
+              "
             >
-              <div class="card-header">{{myName}}的综合素质分布</div>
+              <div class="card-header;margin-bottom:36px">
+                {{ myName }}的综合成绩分布
+              </div>
               <Radar
-                  :chart-options="chartOptions"
-                  :chart-data="chartData"
-                  :chart-id="chartId"
-                  :dataset-id-key="datasetIdKey"
-                  style="width: 400px"
+                v-if="studyChartLoaded"
+                :chart-options="studyChartOptions"
+                :chart-data="studyChartData"
+                :chartId="studyChartId"
+                style="width: 400px"
+              />
+            </div>
+          </el-card>
+          <el-card class="box-card">
+            <div
+              style="
+                display: flex;
+                flex-direction: column;
+                align-content: center;
+                align-items: center;
+              "
+            >
+              <div class="card-header;margin-bottom:36px">
+                {{ myName }}的生活重心分布
+              </div>
+              <PolarArea
+                v-if="lifeChartLoaded"
+                :chart-options="lifeChartOptions"
+                :chart-data="lifeChartData"
+                :chartId="lifeChartId"
+                style="width: 400px"
               />
             </div>
           </el-card>
           <el-button style="margin-top: 12px" size="primary" @click="downPDF()"
-          >下载简历
-          </el-button
-          >
+            >下载简历
+          </el-button>
         </div>
       </el-main>
     </el-container>
@@ -111,26 +133,28 @@
 
 <script>
 import Navi from "@/components/Navi";
-import {getStudentIntroduceData} from "@/service/genServ.js";
-import {downloadPost} from "@/service/genServ.js";
-import {Radar} from "vue-chartjs";
+import { getStudentIntroduceData } from "@/service/genServ.js";
+import { downloadPost } from "@/service/genServ.js";
+import { Radar, PolarArea } from "vue-chartjs";
 import {
   Chart as ChartJS,
   Title,
   Tooltip,
   Legend,
+  ArcElement,
   PointElement,
   RadialLinearScale,
   LineElement,
 } from "chart.js";
 
 ChartJS.register(
-    Title,
-    Tooltip,
-    Legend,
-    PointElement,
-    RadialLinearScale,
-    LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  PointElement,
+  RadialLinearScale,
+  LineElement
 );
 
 export default {
@@ -138,15 +162,20 @@ export default {
   components: {
     Navi,
     Radar,
+    PolarArea,
   },
   props: {
-    chartId: {
+    studyChartId: {
       type: String,
       default: "radar-chart",
     },
-    datasetIdKey: {
+    studyDatasetIdKey: {
       type: String,
       default: "label",
+    },
+    lifeChartId: {
+      type: String,
+      default: "polar-area-chart",
     },
     height: {
       type: Number,
@@ -154,32 +183,46 @@ export default {
     },
     width: {
       type: Number,
-      default: 400
+      default: 400,
     },
   },
   created() {
     this.objectPush = this.$route.query;
-    getStudentIntroduceData({...this.objectPush}).then((res) => {
+    getStudentIntroduceData({ ...this.objectPush }).then((res) => {
       this.myName = res.data.data.myName;
       this.overview = res.data.data.overview;
       this.img_url = res.data.data.img_url;
+      this.studyChartLoaded = "true";
+      this.lifeChartLoaded = "true";
+      this.studyChartData.labels = res.data.data.studyChartLabels;
+      this.studyChartData.datasets = res.data.data.studyChartData;
+      this.lifeChartData.labels = res.data.data.lifeChartLabels;
+      this.lifeChartData.datasets = res.data.data.lifeChartData;
+      this.lifeChartData.backgroundColor = res.data.data.lifeChartColors;
       this.attachList = res.data.data.attachList;
     });
   },
   data() {
     return {
-      chartData: {
-        labels: ["Running", "Swimming", "Eating", "Cycling"],
-        datasets: [
-          {
-            data: [20, 10, 4, 2],
-          },
-        ],
+      studyChartData: {
+        labels: [],
+        datasets: [{}],
       },
-      chartOptions: {
+      studyChartOptions: {
         responsive: true,
-        maintainAspectRatio: true
+        maintainAspectRatio: true,
       },
+      studyChartLoaded: "false",
+      lifeChartData: {
+        labels: [],
+        datasets: [{}],
+      },
+      lifeChartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      },
+      lifeChartLoaded: "false",
+      lifeChartColors:"",
       objectPush: "",
       myName: "",
       overview: "",
@@ -189,9 +232,9 @@ export default {
   methods: {
     downPDF() {
       downloadPost(
-          "http:/localhost:9090/api/teach/getStudentIntroducePdf",
-          "个人简历",
-          {...this.objectPush}
+        "http:/localhost:9090/api/teach/getStudentIntroducePdf",
+        "个人简历",
+        { ...this.objectPush }
       );
     },
   },
